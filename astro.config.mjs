faf2f1b60e6cef0d428455ef9a9540d82fd65672
Svitlana Tsupryk <stsupryk@gmail.com>
2026-06-24 14:41:59 +0300

    Added fonts.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,28 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
 	output: 'static',
 	site: 'https://SvitlanaTsupryk-jul18.github.io',
 	base: '/Astro-project',
-});
+    fonts: [{
+        provider: fontProviders.google(),
+        name: "JetBrains Mono",
+        cssVariable: "--font-jetbrains-mono",
+        weights: [300, 700],
+        subsets: ["cyrillic"],
+      },
+      {
+        provider: fontProviders.local(),
+        name: "Fixel Text",
+        cssVariable: "--font-fixel-text",
+        options: {
+          variants: [
+            { weight: 400, style: "normal", src: ["./src/assets/fonts/FixelText-Regular.woff2"] },
+            { weight: 600, style: "normal", src: ["./src/assets/fonts/FixelText-SemiBold.woff2"] },
+            { weight: 700, style: "normal", src: ["./src/assets/fonts/FixelText-Bold.woff2"] },
+          ],
+        },
+      }],
+},);
